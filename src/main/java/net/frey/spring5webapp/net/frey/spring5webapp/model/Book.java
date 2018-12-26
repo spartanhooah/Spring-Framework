cd545e7a316cd75c75db292504f9b02a27722058
Ben Frey <spartanhooah@gmail.com>
2018-12-26 14:07:14 -0600

    Use Thymeleaf to create a template page for a book list

--- src/main/java/net/frey/spring5webapp/net/frey/spring5webapp/model/Book.java
+++ src/main/java/net/frey/spring5webapp/net/frey/spring5webapp/model/Book.java
@@ -26,6 +26,16 @@ public class Book {
     inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
+    public Book() {
+    }
+
+    public Book(String title, String isbn, Publisher publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+
     public Book(String title, String isbn, Publisher publisher) {
         this.title = title;
         this.isbn = isbn;
