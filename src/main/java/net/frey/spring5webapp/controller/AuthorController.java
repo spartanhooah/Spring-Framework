9fe14abbafd159a6939b9d24541f726ed0e397b6
Ben Frey <spartanhooah@gmail.com>
2018-12-31 11:14:48 -0600

    Create an Authors list page

---
+++ src/main/java/net/frey/spring5webapp/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package net.frey.spring5webapp.controller;
+
+import net.frey.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors";
+    }
+}
